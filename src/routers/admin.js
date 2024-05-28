d3337d05a229764796b6f3148d07574e6d458eb6
Amanvir <amanvir.singh880@gmail.com>
2024-05-28 19:22:27 -0400

    Complete App

--- src/routers/admin.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { init } = require('../data/init');
-
-router.get('/admin/init', async (req, res) => {
-    await init();
-    res.json({msg: 'DB initialized'});
-});
-
-module.exports = router;
